d1ebf9dc7d9ebc38d62034933462c7e43856ec55
rojuu <roju269@gmail.com>
2017-07-28 21:40:16 +0300

    Moving and rotating platform baisikli done

---
+++ Assets/RotatingPlatform.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RotatingPlatform : MonoBehaviour
+{
+    public float radius;
+    Vector3 center;
+    float angle;
+    public float speed;
+
+    public float lerpTime = 3f;
+    float currentLerpTime = 0;
+
+    void Start()
+    {
+        center = transform.position;
+    }
+
+    void Update()
+    {
+        //point on a circle where center is (x0,y0)
+        //(x0 + r cos theta, y0 + r sin theta)
+
+        angle += speed * Time.deltaTime;
+
+        transform.position =
+            new Vector3(
+                center.x + radius * Mathf.Cos(angle),
+                center.y + radius * Mathf.Sin(angle)
+        );
+
+        angle = angle % 360;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+            return;
+
+        Handles.DrawWireDisc(transform.position, Vector3.forward, radius);
+    }
+}
