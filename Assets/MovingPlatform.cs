6b8f5fb766a3264e3ecaaa1f07e0af13b431c8e3
rojuu <roju269@gmail.com>
2017-07-28 21:08:53 +0300

    First actual commit. hoolyy

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    public Vector3 targetPosition;
+    private Vector3 startPosition;
+    
+    public float lerpTime = 3f;
+    float currentLerpTime = 0;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        targetPosition = transform.position + targetPosition;
+    }
+
+    private void Update()
+    {
+        currentLerpTime += Time.deltaTime;
+        float t = currentLerpTime / lerpTime;
+        t = t * t * (3f - 2f * t);
+        transform.position = Vector3.Lerp(startPosition, targetPosition, t);
+        if (Util.VecAlmostEqual(transform.position, targetPosition, 0.001f))
+        {
+            Vector3 temp = targetPosition;
+            targetPosition = startPosition;
+            startPosition = temp;
+            currentLerpTime = 0;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+            return;
+
+        Handles.DrawSolidRectangleWithOutline(
+            new Rect(
+                transform.position.x + (targetPosition.x - transform.localScale.x / 2),
+                transform.position.y + (targetPosition.y - transform.localScale.y / 2),
+                transform.localScale.x,
+                transform.localScale.y
+            ),
+            Color.yellow, Color.red
+        );
+        Handles.color = Color.yellow;
+        Handles.DrawLine(transform.position, transform.position + targetPosition);
+    }
+}
---
+++ Assets/Util.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Util
+{
+    public static bool VecAlmostEqual(Vector3 a, Vector3 b, float accuracy)
+    {
+        return Vector3.SqrMagnitude(a - b) < accuracy;
+    }
+}
