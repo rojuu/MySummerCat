1ce2f1809f0825cebfdb0f761c0791c72427e9d2
rojuu <roju269@gmail.com>
2017-07-29 14:32:08 +0300

    Kill trigger

--- Assets/Scripts/KillTrigger.cs
+++ Assets/Scripts/KillTrigger.cs
@@ -1,13 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class KillTrigger : MonoBehaviour {
 
+    public Vector3 spawnPoint;
+    
+    private void Start() {
+        spawnPoint = spawnPoint + transform.position;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) {
-        print("Hehee");
         if (collision.tag == "Player") {
-            SpawnManager.ResetPlayer();
+            collision.transform.position = spawnPoint;
         }
     }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos() {
+        if (Application.isPlaying)
+            return;
+
+        Handles.DrawSolidRectangleWithOutline(
+            new Rect(
+                transform.position.x + (spawnPoint.x - 0.5f),
+                transform.position.y + (spawnPoint.y - 0.5f),
+                1, 1
+            ),
+            Color.green, Color.yellow
+        );
+
+        Handles.DrawSolidRectangleWithOutline(
+            new Rect(
+                transform.position.x - transform.localScale.x / 2,
+                transform.position.y - transform.localScale.y/ 2,
+                transform.localScale.x,
+                transform.localScale.y
+            ),
+            new Color(0, 0, 0, 0), Color.green
+        );
+
+
+        Handles.color = Color.green;
+        Handles.DrawLine(transform.position, transform.position + spawnPoint);
+    }
+#endif
 }
