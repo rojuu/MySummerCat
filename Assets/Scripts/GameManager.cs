f5b291b65cff748818b5fb350e504e0c2520459b
rojuu <roju269@gmail.com>
2017-07-30 12:40:28 +0300

    Pickups

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameManager {
+    private static int score;
+    public static int Score {
+        get {
+            return score;
+        }
+        set {
+            EventManager.TriggerOnScoreChanged(score, value);
+            score = value;
+        }
+    }
+}
